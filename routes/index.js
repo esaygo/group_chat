bb5cedc10e54d6faf684fa371e32d2ba6fb2d07b
esaygo <elena.saygo@gmail.com>
2016-05-27 19:17:05 -0700

    most of functionalities implemented

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ module.exports = function(app, server){
   //this gets the socket.io module
 var io = require("socket.io").listen(server);
 var existing_users = [];
+var messages = [];
 var session = require('express-session');
 
 app.use(session({resave: true, saveUninitialized: true, secret: "ariel"}));
@@ -14,42 +15,59 @@ app.use(session({resave: true, saveUninitialized: true, secret: "ariel"}));
   });
 
   // Whenever a connection event happens (the connection event is built in) run the following code
-  io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function(socket) {
 
-    socket.join('group_chat');
-    socket.on('disconnect', function(){socket.leave('group_chat') });
+      socket.join('group_chat');
 
-    socket.on("got_new_user", function(data){
-      socket_id = socket.id.replace(/[/#]/g,"");
-      console.log("server id: " + socket_id);
-      //broadcast to all users
-      io.to('group_chat').emit('new_user', {
-                              new_name: data.name,
-                              id: socket_id
-                            });
-      //existing_users.id = session_id;
-      existing_users.push({
-        name: data.name,
-        id: socket_id
-      });
+      socket.on("got_new_user", function(data){
+        socket_id = socket.id.replace(/[/#]/g,"");
+        socket.username = data.name;
+        console.log(socket.username);
+        //console.log("server id: " + socket_id);
+        if(existing_users.length > 0) {
+          socket.emit("existing_users", {"other_messages": messages});
+        }
+        //broadcast to all users
+        io.to('group_chat').emit('new_user', {
+                                new_name: data.name,
+                                id: socket_id
+                              });
+        //existing_users.id = session_id;
+        existing_users.push({
+          name: socket.username,
+          id: socket_id
+        });
+      });//end of "got_new_user"
+
+
 
       socket.on('new_message', function(data) {
-          console.log(data.sid,data.message);
+          //console.log(data.sid,data.message);
           for(var i = 0; i < existing_users.length; i++) {
             if(existing_users[i].id === data.sid){
-              existing_users[i].message = data.message;
-              socket.emit("existing_users", {"other_users": existing_users});
+              //existing_users[i].message = data.message;
+              var user_name = existing_users[i].name;
             }
           }
+          console.log(data.message);
+          console.log(user_name);
+          io.to('group_chat').emit('latest_message', {
+                                    latest_message: data.message,
+                                    name: user_name
+                                  });
+          messages.push({
+            user: user_name,
+            message: data.message
+          });
           console.log(existing_users);
+          console.log(messages);
       });
 
+  socket.on('disconnect', function(){
+    socket.leave('group_chat');
+    io.to('group_chat').emit('user_disconected', {user: socket.username})
 
-
-
-
-    });
-
+  });
 
   }); //end of connection
 
